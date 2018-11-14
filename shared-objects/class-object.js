1e46ce40bb994dc86dc918b275e8a908c55d3205
Philipp Spandl <ph.spandl@gmx.de>
2018-11-14 19:20:39 +0100

    Class object added

---
+++ shared-objects/class-object.js
@@ -0,0 +1,11 @@
+function Class(id, name, subjectArea, user, channel) {
+    this.id = id,
+    this.name = name,
+    this.subjectArea = subjectArea,
+    this.user = user,
+    this.channel = channel
+}
+
+module.exports = {
+    Class : Class
+}
